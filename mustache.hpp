0dd6a3c81756d708b8a7e13944b4dacf31bea35b
Kevin Wojniak <kainjow@kainjow.com>
2015-04-12 10:01:22 -0700

    Replace Xcode project with CMake list file. Move Mustache implementation into a separate header file

--- mustache.hpp
+++ mustache.hpp
@@ -1,10 +1,11 @@
 //
-//  main.cpp
 //  Mustache
-//
 //  Created by Kevin Wojniak on 4/11/15.
 //  Copyright (c) 2015 Kevin Wojniak. All rights reserved.
 //
+
+#ifndef MUSTACHE_HPP
+#define MUSTACHE_HPP
 
 #include <iostream>
 #include <sstream>
@@ -521,33 +522,4 @@ private:
 
 } // namespace
 
-int main() {
-    
-    //Mustache::Mustache<std::wstring> templw(L"");
-    
-    using Data = Mustache::Data<std::string>;
-    Data data;
-    data.set("name", "Kevin");
-    data.set("dayOfWeek", "Monday");
-    Data list(Data::Type::List);
-    for (auto &name : {"Peter", "Paul", "Mary"}) {
-        Data item;
-        item.set("name", name);
-        list.push_back(item);
-    }
-    data.set("names", list);
-    data.set("html", "<b>\"Bold\"</b>");
-
-    std::string input = "{{html}}{{{html}}}{{#names}}secret::{{/names}}Hello {{name}}! Today is {{dayOfWeek}}.";
-    Mustache::Mustache<std::string> templ(input);
-    if (!templ.isValid()) {
-        std::cout << "ERROR: " << templ.errorMessage() << std::endl;
-        return 0;
-    }
-    
-    std::stringstream ss;
-    templ.render(ss, data);
-    std::cout << ss.str() << std::endl;
-    
-    return 0;
-}
+#endif // MUSTACHE_HPP
