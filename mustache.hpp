939cbd988bc03e90658dad2840c4dda3cf0e81dd
Kevin Wojniak <kainjow@users.noreply.github.com>
2017-04-20 20:31:05 -0700

    Disable copying basic_mustache

--- mustache.hpp
+++ mustache.hpp
@@ -333,6 +333,11 @@ public:
         parse(input, ctx);
     }
     
+    basic_mustache(const basic_mustache&) = delete;
+    basic_mustache& operator= (const basic_mustache&) = delete;
+    basic_mustache(basic_mustache&&) = default;
+    basic_mustache& operator= (basic_mustache&&) = default;
+
     bool is_valid() const {
         return errorMessage_.empty();
     }
