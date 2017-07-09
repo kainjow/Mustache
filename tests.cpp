c2699784e87b053904fd09faebbe6a6e5797fb36
Kevin Wojniak <kainjow@users.noreply.github.com>
2017-07-08 22:04:42 -0700

    Add tests for lambda copy constructor

--- tests.cpp
+++ tests.cpp
@@ -394,6 +394,23 @@ TEST_CASE("data") {
         CHECK(lambda3.is_invalid());
     }
 
+    SECTION("lambda_copy_ctor") {
+        data l1{lambda{[](const std::string&){ return ""; }}};
+        auto l2{l1};
+        CHECK(l1.is_lambda());
+        CHECK(l2.is_lambda());
+        CHECK(l1.lambda_value()("") == l2.lambda_value()(""));
+    }
+
+    SECTION("lambda2_copy_ctor") {
+        data l1{lambda2{[](const std::string&, const renderer& r){ return r(""); }}};
+        auto l2{l1};
+        CHECK(l1.is_lambda2());
+        CHECK(l2.is_lambda2());
+        const auto r = [](const std::string& text) { return text; };
+        CHECK(l1.lambda2_value()("", r) == l2.lambda2_value()("", r));
+    }
+
 }
 
 TEST_CASE("errors") {
