f460280feb0dd3e34d8910add488a20fbedfb397
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-07-22 23:15:34 -0700

    Standalone lines tests pass, but broke 2 others

--- mustache.hpp
+++ mustache.hpp
@@ -501,7 +501,7 @@ template <typename string_type>
 class line_buffer_state {
 public:
     string_type data;
-    bool contained_tag = false;
+    bool contained_section_tag = false;
 
     bool is_empty_or_contains_only_whitespace() const {
         for (const auto ch : data) {
@@ -515,7 +515,7 @@ public:
 
     void clear() {
         data.clear();
-        contained_tag = false;
+        contained_section_tag = false;
     }
 };
 
@@ -965,7 +965,7 @@ private:
         // if the line had tags in it, and also if the line is now empty or
         // contains whitespace only. if this situation is true, skip the line.
         bool output = true;
-        if (ctx.line_buffer.contained_tag && ctx.line_buffer.is_empty_or_contains_only_whitespace()) {
+        if (ctx.line_buffer.contained_section_tag && ctx.line_buffer.is_empty_or_contains_only_whitespace()) {
             output = false;
         }
         if (output) {
@@ -977,13 +977,9 @@ private:
         ctx.line_buffer.clear();
     }
     
-    void render_result(const render_handler& handler, context_internal<string_type>& ctx, const string_type& text, bool from_tag) const {
-        (void)ctx;
-        if (from_tag) {
-            //ctx.line_buffer.contained_tag = true;
-        }
-        //ctx.line_buffer.data.append(text);
-        handler(text);
+    void render_result(const render_handler& handler, context_internal<string_type>& ctx, const string_type& text) const {
+        ctx.line_buffer.data.append(text);
+        //handler(text);
         (void)handler;
     }
 
@@ -992,7 +988,7 @@ private:
             if (comp.is_newline()) {
                 render_current_line(handler, ctx, &comp);
             } else {
-                render_result(handler, ctx, comp.text, false);
+                render_result(handler, ctx, comp.text);
             }
             return component<string_type>::walk_control::walk;
         }
@@ -1015,7 +1011,13 @@ private:
                             return component<string_type>::walk_control::stop;
                         }
                     } else if (!var->is_false() && !var->is_empty_list()) {
+                        // account for the section begin tag
+                        ctx.line_buffer.contained_section_tag = true;
+                        
                         render_section(handler, ctx, comp, var);
+                        
+                        // ctx may have been cleared. account for the section end tag
+                        ctx.line_buffer.contained_section_tag = true;
                     }
                 }
                 return component<string_type>::walk_control::skip;
@@ -1098,9 +1100,9 @@ private:
         };
         if (var->is_lambda2()) {
             const basic_renderer<string_type> renderer{render, render2};
-            render_result(handler, ctx, var->lambda2_value()(text, renderer), false);
+            render_result(handler, ctx, var->lambda2_value()(text, renderer));
         } else {
-            render_result(handler, ctx, render(var->lambda_value()(text)), false);
+            render_result(handler, ctx, render(var->lambda_value()(text)));
         }
         return error_message_.empty();
     }
@@ -1108,7 +1110,7 @@ private:
     bool render_variable(const render_handler& handler, const basic_data<string_type>* var, context_internal<string_type>& ctx, bool escaped) {
         if (var->is_string()) {
             const auto& varstr = var->string_value();
-            render_result(handler, ctx, escaped ? escape_(varstr) : varstr, false);
+            render_result(handler, ctx, escaped ? escape_(varstr) : varstr);
         } else if (var->is_lambda()) {
             const render_lambda_escape escape_opt = escaped ? render_lambda_escape::escape : render_lambda_escape::unescape;
             return render_lambda(handler, var, ctx, escape_opt, {}, false);
